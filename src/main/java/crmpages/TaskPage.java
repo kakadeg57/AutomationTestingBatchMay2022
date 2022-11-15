3b9af816c26758efa403a7319abb11c60278751b
Ajinkya Herole <ajinkyagsh@gmail.com>
2022-11-15 22:33:58 +0530

    Adding task page and task page test

---
+++ src/main/java/crmpages/TaskPage.java
@@ -0,0 +1,10 @@
+package crmpages;
+
+public class TaskPage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/test/java/crmtest/TaskPageTest.java
@@ -0,0 +1,10 @@
+package crmtest;
+
+public class TaskPageTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
